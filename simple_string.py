73c3df4763f572a6be4cf3ae175e8be8f87fb286
TH <tommyhh04.kodehode@gmail.com>
2024-09-26 10:07:28 +0200

    Made simple string commands

---
+++ simple_string.py
@@ -0,0 +1,29 @@
+#Making simple string commands as exercise
+
+
+#Notice the use of single or double quotes
+
+
+# "This is a string"
+# 'This is also a string'
+
+
+#Printing a name in text case(?)(Upper or lowercase. First letter in upper case)
+#Remember to use () at the end of title or code wont run example =( name.title() )
+katt_navn = "nusse katt"
+name_01 = "Tommy"
+
+print(katt_navn.title())
+print(katt_navn.upper())
+
+print(name_01.lower())
+
+
+#Using variables in a string
+
+first_name = "ada"
+last_name = "lovelace"
+
+full_name =f"Hello {first_name} {last_name}"
+
+print(full_name.title())
